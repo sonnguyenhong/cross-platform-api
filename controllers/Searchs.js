47c78a91084decd69be65b1bb290abba9f5b3f58
sonnguyenhong <sonnguyenhong291@gmail.com>
2023-02-14 09:49:24 +0700

    app: add searchs api

--- controllers/Searchs.js
+++ controllers/Searchs.js
@@ -1,30 +1,39 @@
-const ChatModel = require("../models/Chats");
-const FriendModel = require("../models/Friends");
-const Messages = require("../models/Messages");
-const MessagesModel = require("../models/Messages");
-const httpStatus = require("../utils/httpStatus");
-const UserModel = require("../models/Users");
-const friendController = require("./Friends")
+const ChatModel = require('../models/Chats');
+const FriendModel = require('../models/Friends');
+const Messages = require('../models/Messages');
+const MessagesModel = require('../models/Messages');
+const SearchModel = require('../models/Searchs');
+const httpStatus = require('../utils/httpStatus');
+const UserModel = require('../models/Users');
+const friendController = require('./Friends');
 const searchController = {};
 
 searchController.search = async (req, res, next) => {
     try {
         let key = req.params.key;
-        userId = req.userId;
+        let userId = req.userId;
+
+        let searchHistoryMatch = await SearchModel.findOne({ key });
+        if (searchHistoryMatch) {
+            await SearchModel.updateOne({ key }, { lastSearchTime: Date.now() });
+        } else {
+            await SearchModel.create({
+                user: userId,
+                key,
+                lastSearchTime: Date.now(),
+            });
+        }
+
         let friendList = [];
         let peopleList = [];
         let messageList = [];
         let friends = await FriendModel.find({
             $and: [
                 {
-                    $or: [
-                        { sender: userId },
-                        { receiver: userId }
-                    ]
+                    $or: [{ sender: userId }, { receiver: userId }],
                 },
-                { status: "1" }
-            ]
-
+                { status: '1' },
+            ],
         }).populate('messsages');
 
         let friendIds = [];
@@ -41,12 +50,14 @@ searchController.search = async (req, res, next) => {
                 { _id: { $in: friendIds } },
                 {
                     $or: [
-                        { username: { "$regex": key, "$options": "i" } },
-                        { phonenumber: { "$regex": key, "$options": "i" } }
-                    ]
-                }
-            ]
-        }).populate('avatar').limit(10);
+                        { username: { $regex: key, $options: 'i' } },
+                        { phonenumber: { $regex: key, $options: 'i' } },
+                    ],
+                },
+            ],
+        })
+            .populate('avatar')
+            .limit(10);
 
         friendIds.push(userId);
 
@@ -55,57 +66,59 @@ searchController.search = async (req, res, next) => {
                 { _id: { $nin: friendIds } },
                 {
                     $or: [
-                        { username: { "$regex": key, "$options": "i" } },
-                        { phonenumber: { "$regex": key, "$options": "i" } }
-                    ]
-                }
-            ]
-        }).populate('avatar').limit(5);
+                        { username: { $regex: key, $options: 'i' } },
+                        { phonenumber: { $regex: key, $options: 'i' } },
+                    ],
+                },
+            ],
+        })
+            .populate('avatar')
+            .limit(5);
 
         let temp = [];
 
-        for(let i =0; i< peopleList.length; i++){
+        for (let i = 0; i < peopleList.length; i++) {
             let friendStatus = await friendController.getFriendStatus(userId, peopleList[i]._id);
             let object = {
-                gender : peopleList[i].gender,
-                blocked_inbox : peopleList[i].blocked_inbox,
-                blocked_diary : peopleList[i].blocked_diary,
-                _id : peopleList[i]._id,
-                phonenumber : peopleList[i].phonenumber,
-                username : peopleList[i].username,
-                avatar : peopleList[i].avatar,
-                cover_image : peopleList[i].avatar,
-                friendStatus : friendStatus
-            }
-            temp.push(object)
+                gender: peopleList[i].gender,
+                blocked_inbox: peopleList[i].blocked_inbox,
+                blocked_diary: peopleList[i].blocked_diary,
+                _id: peopleList[i]._id,
+                phonenumber: peopleList[i].phonenumber,
+                username: peopleList[i].username,
+                avatar: peopleList[i].avatar,
+                cover_image: peopleList[i].avatar,
+                friendStatus: friendStatus,
+            };
+            temp.push(object);
         }
         peopleList = temp;
 
         let messages = await MessagesModel.find({
             $and: [
                 {
-                    $or: [
-                        { senderId: userId },
-                        { receiverId: userId }
-                    ]
+                    $or: [{ senderId: userId }, { receiverId: userId }],
                 },
-                { content: { "$regex": key, "$options": "i" } },
-            ]
-        }).populate({
-            path: 'senderId',
-            model: 'Users',
-            populate: {
-                path: 'avatar',
-                model: 'Documents'
-            }
-        }).populate({
-            path: 'receiverId',
-            model: 'Users',
-            populate: {
-                path: 'avatar',
-                model: 'Documents'
-            }
-        }).limit(10);
+                { content: { $regex: key, $options: 'i' } },
+            ],
+        })
+            .populate({
+                path: 'senderId',
+                model: 'Users',
+                populate: {
+                    path: 'avatar',
+                    model: 'Documents',
+                },
+            })
+            .populate({
+                path: 'receiverId',
+                model: 'Users',
+                populate: {
+                    path: 'avatar',
+                    model: 'Documents',
+                },
+            })
+            .limit(10);
 
         for (let i = 0; i < messages.length; i++) {
             let message = {};
@@ -114,9 +127,9 @@ searchController.search = async (req, res, next) => {
             message.chatId = messages[i].chatId;
             message.senderId = messages[i].senderId;
             if (messages[i].senderId._id == userId) {
-                message["friend"] = messages[i].receiverId;
+                message['friend'] = messages[i].receiverId;
             } else {
-                message["friend"] = messages[i].senderId;
+                message['friend'] = messages[i].senderId;
             }
 
             messageList.push(message);
@@ -126,16 +139,74 @@ searchController.search = async (req, res, next) => {
             data: {
                 friends: friendList,
                 people: peopleList,
-                messages: messageList
-            }
+                messages: messageList,
+            },
         });
-
     } catch (e) {
         return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            message: e.message
+            message: e.message,
         });
     }
-}
+};
 
+searchController.getSearchHistoryKeys = async (req, res, next) => {
+    try {
+        // console.log('hi');
+        let userId = req.userId;
+        let searchKeyList = [];
+
+        let searchesHistoryMatch = await SearchModel.find({ user: userId });
+        // console.log(searchesHistoryMatch);
+        if (searchesHistoryMatch) {
+            searchesHistoryMatch.forEach((searchKey) => {
+                searchKeyList.push({
+                    id: searchKey._id,
+                    key: searchKey.key,
+                    last_search: searchKey.lastSearchTime,
+                });
+            });
+        }
+
+        return res.status(httpStatus.OK).json({
+            data: {
+                keys: searchKeyList,
+            },
+        });
+    } catch (e) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+            message: e.message,
+        });
+    }
+};
+
+searchController.deleteSearchHistoryKey = async (req, res, next) => {
+    try {
+        // console.log('hi');
+        let userId = req.userId;
+        let searchId = req.query.search_id;
+        let deleteAll = req.query.all;
+
+        if (deleteAll == 1) {
+            await SearchModel.remove({ user: userId });
+            return res.status(httpStatus.OK).json({
+                message: 'Delete search key successful',
+            });
+        } else {
+            let searchHistoryMatch = await SearchModel.findOne({ user: userId, _id: searchId });
+            if (searchHistoryMatch) {
+                await SearchModel.remove({ user: userId, _id: searchId });
+                return res.status(httpStatus.OK);
+            } else {
+                return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                    message: 'Delete search key fail',
+                });
+            }
+        }
+    } catch (e) {
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+            message: e.message,
+        });
+    }
+};
 
 module.exports = searchController;
--- routes/Searchs.js
+++ routes/Searchs.js
@@ -1,13 +1,12 @@
-const searchController = require("../controllers/Searchs");
-const {asyncWrapper} = require("../utils/asyncWrapper");
-const express = require("express");
+const searchController = require('../controllers/Searchs');
+const { asyncWrapper } = require('../utils/asyncWrapper');
+const express = require('express');
 const searchRoutes = express.Router();
-const auth = require("../middlewares/auth");
+const auth = require('../middlewares/auth');
 
-searchRoutes.get(
-    "/:key",
-    auth,
-    asyncWrapper(searchController.search),
-);
+searchRoutes.get('/search-by-key/:key', auth, asyncWrapper(searchController.search));
 
+searchRoutes.get('/history', auth, asyncWrapper(searchController.getSearchHistoryKeys));
+
+searchRoutes.post('/delete-key', auth, asyncWrapper(searchController.deleteSearchHistoryKey));
 module.exports = searchRoutes;
